72f1f5f9efa56941011458c4d90df8a506eb80d0
txuli <127785824+txuli@users.noreply.github.com>
2026-08-12 13:03:59 +0200

    feat: implement save_credentials function and update token verification response structure

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -3,3 +3,14 @@ use serde::{Deserialize, Serialize};
 pub struct CheckToken{
     pub valid:bool,
 }
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ValidResponse{
+    pub valid:bool,
+    pub type_acc: String,
+}
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Settings{
+    pub url: String,
+    pub token:String,
+
+}
