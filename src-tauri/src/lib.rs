5944fe215cbe478c9b4fa24458dfaae37a709814
txuli <127785824+txuli@users.noreply.github.com>
2026-08-13 12:15:57 +0200

    feat: add autostart plugin and update configuration options for background processing

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,10 @@
-
+use serde_json::json;
 use std::fs::File;
 use std::io::BufReader;
-use serde_json::json;
 pub mod models;
 pub use models::CheckToken;
-pub use models::ValidResponse;
 pub use models::Settings;
+pub use models::ValidResponse;
 
 #[tauri::command]
 async fn verify_token(url: &str, token: &str) -> Result<ValidResponse, String> {
@@ -18,17 +17,22 @@ async fn verify_token(url: &str, token: &str) -> Result<ValidResponse, String> {
         .map_err(|err| format!("Error de red: {}", err))?;
 
     if response.status().is_success() {
-        Ok(ValidResponse { valid: true, type_acc:"credential".to_string() })
+        Ok(ValidResponse {
+            valid: true,
+            type_acc: "credential".to_string(),
+        })
     } else {
-        Ok(ValidResponse { valid: false, type_acc:"credential".to_string() })
+        Ok(ValidResponse {
+            valid: false,
+            type_acc: "credential".to_string(),
+        })
     }
 }
 #[tauri::command]
 async fn save_credentials(url: &str, token: &str) -> Result<ValidResponse, String> {
-    
-   let try_save = || -> Result<(), std::io::Error> {
+    let try_save = || -> Result<(), std::io::Error> {
         let file = File::create("config.json")?;
-        let data= json!({
+        let data = json!({
             "url": url,
             "token": token,
         });
@@ -37,24 +41,28 @@ async fn save_credentials(url: &str, token: &str) -> Result<ValidResponse, Strin
     };
 
     match try_save() {
-        Ok(_) => Ok(ValidResponse { valid: true, type_acc:"save".to_string() }),
-        Err(_err) => Ok(ValidResponse { valid: false, type_acc:"save".to_string() }),
+        Ok(_) => Ok(ValidResponse {
+            valid: true,
+            type_acc: "save".to_string(),
+        }),
+        Err(_err) => Ok(ValidResponse {
+            valid: false,
+            type_acc: "save".to_string(),
+        }),
     }
-        
 }
-#[tauri::command]
+/* #[tauri::command]
 async fn load_config() -> Result<Settings, String> {
     let file = File::open("config.json").map_err(|e| e.to_string())?;
     let reader = BufReader::new(file);
-
     let s = serde_json::from_reader(reader).map_err(|e| e.to_string())?;
-
     Ok(s)
-}
+} */
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_autostart::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![verify_token, save_credentials])
         .run(tauri::generate_context!())
--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -1,16 +1,15 @@
 use serde::{Deserialize, Serialize};
 #[derive(Deserialize, Serialize, Debug)]
-pub struct CheckToken{
-    pub valid:bool,
+pub struct CheckToken {
+    pub valid: bool,
 }
 #[derive(Deserialize, Serialize, Debug)]
-pub struct ValidResponse{
-    pub valid:bool,
+pub struct ValidResponse {
+    pub valid: bool,
     pub type_acc: String,
 }
 #[derive(Deserialize, Serialize, Debug)]
-pub struct Settings{
+pub struct Settings {
     pub url: String,
-    pub token:String,
-
+    pub token: String,
 }
